aea6e6b4ed9b01ecc738361b55454dff4170b4f2
Kai Park <applelaptop@Kais-MacBook-Air-3.local>
2024-07-07 01:10:53 -0400

    first commit

---
+++ personal_finance_backend/app.py
@@ -0,0 +1,133 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from datetime import datetime as dt 
+import pandas as pd 
+import numpy as np 
+import os
+
+from personal_finance import PersonalFinance
+
+app = Flask(__name__)
+CORS(app)
+
+# Initialize PersonalFinance with a test user
+finance = PersonalFinance(user_name="kai")
+finance.load()
+
+@app.route('/api/user_name', methods=['GET'])
+def user_name():
+    return jsonify(finance.user_name)
+
+# get all expenses 
+@app.route('/api/get_all_expenses', methods=['GET'])
+def get_all_expenses():
+    global finance
+    data = finance.data
+    # convert dates to string format
+    data['date'] = data['date'].apply(lambda date: str(date)[:10])
+    return data.to_json(orient='index')
+    
+# Fetch monthly totals for a specified month
+@app.route('/api/get_monthly_totals', methods=['GET'])
+def get_monthly_totals():
+    month = request.args.get('month')
+    try:
+        totals = finance.monthly_cat_totals(month)
+        return totals.to_json(orient='records')
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+# Fetch monthly totals in percent format for bar chart purposes
+@app.route('/api/get_monthly_heights', methods=['GET'])
+def get_monthly_heights():
+    month = request.args.get('month')
+    try:
+        totals_df = finance.monthly_cat_totals(month)
+        totals_df['amount'] = totals_df['amount'] / max(totals_df['amount'])
+        return totals_df.to_json(orient='records')
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+# Numerical monthly sum
+@app.route('/api/get_monthly_sum', methods=['GET'])
+def get_monthly_sum():
+    month = request.args.get('month')
+    try:
+        monthly_sum = finance.monthly_cat_totals(month)['amount'].sum()
+        return jsonify({'sum': '{:,.2f}'.format(monthly_sum)})
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+# List of months used internally
+@app.route('/api/months_list', methods=['GET'])
+def months_list():
+    try:
+        months = ['ALL'] + sorted(set(finance._data_month_incl['month']))
+        return jsonify(months)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+# Route to add an expense 
+@app.route('/api/add_expense', methods=['POST'])
+def add_expense():
+    data = request.get_json()
+    date = data.get('date')
+    category = data.get('category')
+    title = data.get('title')
+    amount = data.get('amount')
+    notes = data.get('notes', '')
+
+    if not all([date, category, title, amount]):
+        return jsonify({'error': 'missing required information'}), 400
+    
+    try:
+        finance.new_entry(date=date, category=category, title=title, amount=amount, notes=notes)
+        finance.dump()
+        return jsonify({'message': 'expense add success'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+# Delete an expense entry by index
+@app.route('/api/delete_expense', methods=['POST'])
+def delete_expense():
+    index = request.args.get('index')
+    try:
+        finance.delete_index(index=int(index))
+        finance.dump()
+        return jsonify({'message': 'expense delete success'}), 201
+    except IndexError as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/api/refresh_data', methods=['POST'])
+def refresh_data():
+    global finance
+    finance = PersonalFinance(user_name="kai")
+    finance.load()
+    data = finance.data
+    data['date'] = data['date'].apply(lambda date: str(date)[:10])
+    return data.to_json(orient='records')
+
+@app.route('/api/establish_session', methods=['POST'])
+def establish_session():
+    try:
+        finance.establish_new_session()
+        return jsonify(
+            {'success': f'new session established: {finance.session_id}'}
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                'error': f'failed to establish session {str(e)}'
+            }
+        )
+    
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ personal_finance_backend/personal_finance.py
@@ -0,0 +1,106 @@
+"""
+PersonalFinance class, define with person's name
+attrib .data: a pandas DataFrame with entries, columns: ['date', 'category', 'title', 'amount', 'notes']
+attrib .barchart: barchart in the form of a DataFrame, with the category, category sum ('amount'), and height (log scale of amount)
+
+ ／l、
+（ﾟ､ ｡ ７
+  l  ~ヽ
+  じしf_,)ノ
+"""
+
+import pandas as pd
+from pandas import Period
+from datetime import datetime as dt
+import pytz
+import numpy as np
+import os
+
+class PersonalFinance:
+    def __init__(self, user_name: str) -> None:
+        # used for optimistic locking
+        self.session_id = str(dt.now(pytz.timezone('America/Los_Angeles')))
+
+        """initialize the class with the following attributes:
+            * user_name -- the user's name, used to store information
+            * data -- initialized as an empty dataframe, gets concatenated with new additions
+            * cat_totals -- category totals """
+        self.user_name = user_name
+        # initialize dataframe with these columns
+        self.data = pd.DataFrame(columns=['date', 'category', 'title', 'amount', 'notes', 'session_id'])
+        # dataframe for totals is stored here. also includes month.
+        self._cat_totals = None
+
+    def new_entry(self, date: "DateLike", category: str, title: str, amount: float, notes: str = ' ') -> None:
+        """add a row to self.data"""
+        new_row = pd.DataFrame([{
+            'date': date,
+            'category': category,
+            'title': title,
+            'amount': amount,
+            'notes': notes,
+            'session_id': self.session_id
+        }])
+        self.data = pd.concat([self.data, new_row], ignore_index=True)
+
+    @property
+    def _data_month_incl(self) -> pd.DataFrame:
+        """literally a copy of self.data with specific month column."""
+        df = self.data.copy()
+        df['date'] = pd.to_datetime(df['date'])
+        df['month'] = df['date'].dt.to_period('M')
+        df['month'] = df['month'].apply(lambda month: str(month))
+        return df
+
+    @property
+    def cat_totals(self) -> pd.DataFrame | None:
+        """big dataframe to store totals by category"""
+        if not self._data_month_incl.empty:
+            self._cat_totals = self._data_month_incl.groupby(['month', 'category']).sum(numeric_only=True)
+        return self._cat_totals
+
+    def monthly_cat_totals(self, month: str = 'ALL') -> dict[str, pd.DataFrame]:
+        """filter self.cat_totals for a specified month"""
+        months = list(set(self._data_month_incl['month']))
+        if month == 'ALL':
+            return self.cat_totals.reset_index().groupby('category').sum(numeric_only=True).reset_index()
+        if month not in months:
+            raise ValueError("Invalid Month: {}".format(month))
+        else:
+            return self.cat_totals.loc[month].reset_index()
+
+    def delete_index(self, index: int) -> None:
+        """remove an index from self.data"""
+        if index in self.data.index:
+            self.data = self.data.drop(index=index)
+            self.data = self.data.reset_index(drop=True)[['date', 'category', 'title', 'amount', 'notes', 'session_id']]
+        else:
+            raise IndexError('Index not found')
+
+    def dump(self) -> None:
+        """write self.data to csv file"""
+        latest_written_session = pd.read_csv(f'personal_finance_{self.user_name}.csv')['session_id'].max()
+        if self.session_id <= latest_written_session:
+            # if this instance's session ID is less than the most recent session ID, do not write.
+            raise Exception("Failed to write -- local copy is out of date. Please reload.")
+        else:
+            self.data.to_csv(f'personal_finance_{self.user_name}.csv', index=False)
+
+    def load(self) -> None:
+        """load a csv file"""
+        assert os.path.exists(f'personal_finance_{self.user_name}.csv')
+        read_df = pd.read_csv(f'personal_finance_{self.user_name}.csv')[['date', 'category', 'title', 'amount', 'notes', 'session_id']]
+        if not read_df.empty:
+            self.data = read_df.fillna('')
+        self.data['date'] = self.data['date'].apply(lambda date: date[:10])
+        self.data['date'] = pd.to_datetime(self.data['date'])
+        self.data['amount'] = self.data['amount'].apply(lambda amt: np.round(amt, 2))
+        # update session name
+        self._cat_totals = pd.DataFrame(columns=['category', 'amount'])
+
+    def establish_new_session(self) -> None:
+        # used for version lock
+        self.session_id = str(dt.now(pytz.timezone('America/Los_Angeles')))
+        self.data['session_id'] = self.session_id
+
+
