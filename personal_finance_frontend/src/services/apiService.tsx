f70738783fe7e99aa5fe16cd80b543e9088d41ba
Kai Park <applelaptop@Kais-MacBook-Air-3.local>
2024-07-07 01:47:38 -0400

    dotenv

--- personal_finance_frontend/src/services/apiService.tsx
+++ personal_finance_frontend/src/services/apiService.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:5000/api";
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+console.log(API_BASE_URL)
 
 export const getUserName = async () => {
   const response = await axios.get(`${API_BASE_URL}/user_name`);
